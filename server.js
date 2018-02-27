a500973573a48d907e1c5b7f1f892adbe9eef939
Kevin <kevinwang27@berkeley.edu>
2018-02-27 12:06:06 -0800

    initial setup

---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = '765f0bbad3a24f3c8dbb45b9ad53c233';
+var client_secret = '***REMOVED***';
+var redirect_uri = 'http://localhost:8888/callback';
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+app.get('/login', function(req, res) {
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    var scope = 'playlist-read-private playlist-modify-private playlist-modify-public playlist-read-collaborative';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+app.get('/callback', function(req, res) {
+  
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+  
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+            error: 'state_mismatch'
+        }));
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+  
+        request.post(authOptions, function(error, response, body) {
+            var access_token = body.access_token,
+                refresh_token = body.refresh_token;
+
+            if (!error && response.statusCode === 200) {
+                res.redirect('/#' +
+                querystring.stringify({
+                    access_token: access_token,
+                    refresh_token: refresh_token
+                }));
+            } else {
+            res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+            }
+      });
+    }
+});
+
+console.log('Listening on 8888');
+app.listen(8888);
